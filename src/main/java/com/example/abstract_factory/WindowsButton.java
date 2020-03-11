7d26e4d44b886e5c6c15bab62c60bde7d1d5d346
alayahamza <alaya.hamza@gmail.com>
2020-03-11 15:35:16 +0100

    initial commit

--- src/main/java/com/example/abstract_factory/WindowsButton.java
+++ src/main/java/com/example/abstract_factory/WindowsButton.java
@@ -3,6 +3,6 @@ package com.example.abstract_factory;
 public class WindowsButton implements Button {
 
     public void paint() {
-
+        System.out.println("You have created WindowsButton.");
     }
 }
